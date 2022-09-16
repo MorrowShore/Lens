0cc0730c89461aa666f52cf813ef60434775320e
Alexander <megag96@mail.ru>
2022-09-16 18:20:32 +0300

    Refactoring

--- src/chat_services/trovo.cpp
+++ src/chat_services/trovo.cpp
@@ -1,9 +1,64 @@
 #include "trovo.h"
+#include <QNetworkAccessManager>
+#include <QJsonDocument>
+#include <QJsonObject>
+#include <QJsonValue>
 
-Trovo::Trovo(QSettings &settings_, const QString &settingsGroupPath, QNetworkAccessManager &, QObject *parent)
+Trovo::Trovo(QSettings &settings_, const QString &settingsGroupPath, QNetworkAccessManager &network_, QObject *parent)
     : ChatService(settings_, settingsGroupPath, AxelChat::ServiceType::Trovo, parent)
+    , settings(settings_)
+    , network(network_)
 {
+    getParameter(stream)->setPlaceholder(tr("Link or channel name..."));
 
+    QObject::connect(&socket, &QWebSocket::stateChanged, this, [](QAbstractSocket::SocketState state)
+    {
+        Q_UNUSED(state)
+        qDebug() << Q_FUNC_INFO << ": WebSocket state changed:" << state;
+    });
+
+    QObject::connect(&socket, &QWebSocket::textMessageReceived, this, &Trovo::onWebSocketReceived);
+
+    QObject::connect(&socket, &QWebSocket::connected, this, [this]()
+    {
+        if (state.connected)
+        {
+            state.connected = false;
+            emit connectedChanged(false, lastConnectedChannelName);
+        }
+
+        QJsonObject root;
+        root.insert("type", "AUTH");
+        root.insert("nonce", "nonce");
+
+        QJsonObject data;
+
+        data.insert("token", "token");
+
+        root.insert("data", data);
+
+        sendToWebSocket(QJsonDocument(root));
+
+        emit stateChanged();
+    });
+
+    QObject::connect(&socket, &QWebSocket::disconnected, this, [this]()
+    {
+        if (state.connected)
+        {
+            state.connected = false;
+
+            emit stateChanged();
+            emit connectedChanged(false, lastConnectedChannelName);
+        }
+    });
+
+    QObject::connect(&socket, QOverload<QAbstractSocket::SocketError>::of(&QWebSocket::error), this, [this](QAbstractSocket::SocketError error_)
+    {
+        qDebug() << Q_FUNC_INFO << ": WebSocket error:" << error_ << ":" << socket.errorString();
+    });
+
+    reconnect();
 }
 
 ChatService::ConnectionStateType Trovo::getConnectionStateType() const
@@ -45,5 +100,56 @@ QString Trovo::getStateDescription() const
 
 void Trovo::reconnect()
 {
+    socket.close();
 
+    state = State();
+
+    state.streamId = getStreamId(stream.get());
+
+    if (state.streamId.isEmpty())
+    {
+        emit stateChanged();
+        return;
+    }
+
+    socket.setProxy(network.proxy());
+    socket.open(QUrl("wss://open-chat.trovo.live/chat"));
+
+    emit stateChanged();
+}
+
+void Trovo::onWebSocketReceived(const QString& rawData)
+{
+    qDebug("RECIEVE: " + rawData.toUtf8() + "\n");
+
+    const QJsonObject root = QJsonDocument::fromJson(rawData.toUtf8()).object();
+
+    const QString typeMessage = root.value("type").toString();
+    const QString error = root.value("error").toString();
+
+    if (typeMessage == "RESPONSE")
+    {
+
+    }
+    else
+    {
+        qWarning() << Q_FUNC_INFO << ": unknown message type" << typeMessage << ", raw data =" << rawData;
+    }
+
+    if (!error.isEmpty())
+    {
+        qWarning() << Q_FUNC_INFO << ": error" << error << ", raw data =" << rawData;
+    }
+}
+
+void Trovo::sendToWebSocket(const QJsonDocument &data)
+{
+    qDebug("SEND:" + data.toJson(QJsonDocument::JsonFormat::Compact) + "\n");
+    socket.sendTextMessage(QString::fromUtf8(data.toJson()));
+}
+
+QString Trovo::getStreamId(const QString &stream)
+{
+    //TODO
+    return stream.toLower().trimmed();
 }
--- src/chat_services/trovo.h
+++ src/chat_services/trovo.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "chatservice.h"
+#include <QWebSocket>
 
 class Trovo : public ChatService
 {
@@ -12,5 +13,18 @@ public:
     QString getStateDescription() const override;
     void reconnect() override;
 
+private slots:
+    void onWebSocketReceived(const QString& rawData);
+    void sendToWebSocket(const QJsonDocument& data);
+
+private:
+    static QString getStreamId(const QString& stream);
+
+    QSettings& settings;
+    QNetworkAccessManager& network;
+
+    QWebSocket socket;
+
+    QString lastConnectedChannelName;
 };
 
