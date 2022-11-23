8625e3dcf89d3f58d742b08af63aba6dfdd9450b
Alexander <megag96@mail.ru>
2022-11-23 21:54:46 +0300

    Added apikey.h with template code

---
+++ src/apikeys.h
@@ -0,0 +1,12 @@
+#pragma once
+
+//#define TWITCH_CLIENT_ID "Uncomment and paste Twitch client ID here"
+//#define TROVO_CLIENT_ID "Uncomment and paste Trovo client ID here"
+
+#ifndef TWITCH_CLIENT_ID
+#error("TWITCH_CLIENT_ID not defined")
+#endif
+
+#ifndef TROVO_CLIENT_ID
+#error("TROVO_CLIENT_ID not defined")
+#endif
