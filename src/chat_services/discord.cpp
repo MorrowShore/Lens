005ea9915a4c19e66e080fcd79103f7c8473e972
Alexander <megag96@mail.ru>
2023-02-03 09:30:16 +0300

    Discord: implemented socket connection

--- src/chat_services/discord.cpp
+++ src/chat_services/discord.cpp
@@ -12,6 +12,11 @@ namespace
 static const QString ClientID = OBFUSCATE(DISCORD_CLIENT_ID);
 static const int ServerPort = 8356;
 
+static const int ReconncectPeriod = 3 * 1000;
+static const int HeartbeatOpCode = 1;
+static const int HelloOpCode = 10;
+static const int HeartAckbeatOpCode = 11;
+
 static const QByteArray makeHttpResponse(const QByteArray& data)
 {
     return QString("HTTP/1.1 200 OK\n"
@@ -107,7 +112,70 @@ Discord::Discord(QSettings &settings_, const QString &settingsGroupPath, QNetwor
         });
     });
 
+    QObject::connect(&socket, &QWebSocket::textMessageReceived, this, &Discord::onWebSocketReceived);
+
+    QObject::connect(&socket, &QWebSocket::stateChanged, this, [](QAbstractSocket::SocketState state)
+    {
+        Q_UNUSED(state)
+        qDebug() << Q_FUNC_INFO << ": WebSocket state changed:" << state;
+    });
+
+    QObject::connect(&socket, &QWebSocket::connected, this, [this]()
+    {
+        qDebug() << Q_FUNC_INFO << ": WebSocket connected";
+
+        heartbeatAcknowledgementTimer.setInterval(60 * 10000);
+        heartbeatAcknowledgementTimer.start();
+    });
+
+    QObject::connect(&socket, &QWebSocket::disconnected, this, [this]()
+    {
+        qDebug() << Q_FUNC_INFO << ": WebSocket disconnected";
+        processDisconnected();
+    });
+
+    QObject::connect(&socket, QOverload<QAbstractSocket::SocketError>::of(&QWebSocket::error), this, [this](QAbstractSocket::SocketError error_)
+    {
+        qDebug() << Q_FUNC_INFO << ": WebSocket error:" << error_ << ":" << socket.errorString();
+    });
+
+    QObject::connect(&heartbeatTimer, &QTimer::timeout, this, &Discord::sendHeartbeat);
+
+    QObject::connect(&heartbeatAcknowledgementTimer, &QTimer::timeout, this, [this]()
+    {
+        if (socket.state() != QAbstractSocket::SocketState::ConnectedState)
+        {
+            heartbeatAcknowledgementTimer.stop();
+            return;
+        }
+
+        qDebug() << Q_FUNC_INFO << "heartbeat acknowledgement timeout, disconnect";
+        socket.close();
+    });
+
+    QObject::connect(&timerReconnect, &QTimer::timeout, this, [this]()
+    {
+        if (!enabled.get())
+        {
+            return;
+        }
+
+        if (socket.state() == QAbstractSocket::SocketState::ConnectedState ||
+            socket.state() == QAbstractSocket::SocketState::ConnectingState)
+        {
+            return;
+        }
+
+        if (!state.connected && isAuthorized())
+        {
+            reconnect();
+        }
+    });
+    timerReconnect.start(ReconncectPeriod);
+
     updateAuthState();
+
+    reconnect();
 }
 
 ChatService::ConnectionStateType Discord::getConnectionStateType() const
@@ -154,7 +222,72 @@ QString Discord::getStateDescription() const
 
 void Discord::reconnectImpl()
 {
+    socket.close();
 
+    state = State();
+    info = Info();
+
+    processDisconnected();
+
+    if (!isAuthorized() || !enabled.get())
+    {
+        return;
+    }
+
+    socket.setProxy(network.proxy());
+    socket.open(QUrl("wss://gateway.discord.gg/?v=10&encoding=json"));
+}
+
+void Discord::onWebSocketReceived(const QString &rawData)
+{
+    qDebug("\nreceived:\n" + rawData.toUtf8() + "\n");
+
+    if (!enabled.get())
+    {
+        return;
+    }
+
+    const QJsonObject root = QJsonDocument::fromJson(rawData.toUtf8()).object();
+    if (root.contains("s"))
+    {
+        const QJsonValue s = root.value("s");
+        if (!s.isNull())
+        {
+            info.lastSequenceNumber = s;
+        }
+    }
+
+    const int opCode = root.value("op").toInt();
+
+    if (opCode == HelloOpCode)
+    {
+        parseHello(root.value("d").toObject());
+    }
+    else if (opCode == HeartAckbeatOpCode)
+    {
+        heartbeatAcknowledgementTimer.setInterval(info.heartbeatInterval * 1.5);
+        heartbeatAcknowledgementTimer.start();
+    }
+    else
+    {
+        qWarning() << Q_FUNC_INFO << "unknown op code" << opCode;
+    }
+}
+
+void Discord::sendHeartbeat()
+{
+    if (socket.state() != QAbstractSocket::SocketState::ConnectedState)
+    {
+        return;
+    }
+
+    const QJsonObject data =
+    {
+        { "op", QJsonValue(HeartbeatOpCode) },
+        { "d", info.lastSequenceNumber  },
+    };
+
+    send(data);
 }
 
 bool Discord::isAuthorized() const
@@ -181,4 +314,33 @@ void Discord::updateAuthState()
     }
 
     emit stateChanged();
+}
+
+void Discord::processDisconnected()
+{
+    if (state.connected)
+    {
+        state.connected = false;
+        emit stateChanged();
+        emit connectedChanged(false, QString());
+    }
+}
+
+void Discord::send(const QJsonObject &data)
+{
+    qDebug("\nsend:\n" + QJsonDocument(data).toJson() + "\n");
+
+    socket.sendTextMessage(QString::fromUtf8(QJsonDocument(data).toJson()));
+}
+
+void Discord::parseHello(const QJsonObject &data)
+{
+    info.heartbeatInterval = data.value("heartbeat_interval").toInt(30000);
+    if (info.heartbeatInterval < 1000)
+    {
+        info.heartbeatInterval = 1000;
+    }
+
+    heartbeatTimer.setInterval(info.heartbeatInterval);
+    heartbeatTimer.start();
 }
--- src/chat_services/discord.h
+++ src/chat_services/discord.h
@@ -2,6 +2,8 @@
 
 #include "chatservice.h"
 #include <QTcpServer>
+#include <QWebSocket>
+#include <QTimer>
 
 class Discord : public ChatService
 {
@@ -15,18 +17,40 @@ public:
 protected:
     void reconnectImpl() override;
 
+private slots:
+    void onWebSocketReceived(const QString& rawData);
+    void sendHeartbeat();
+
 private:
     bool isAuthorized() const;
     void updateAuthState();
+    void processDisconnected();
+
+    void send(const QJsonObject& data);
+    void parseHello(const QJsonObject& data);
+
+    struct Info
+    {
+        int heartbeatInterval = 30000;
+        QJsonValue  lastSequenceNumber;
+    };
 
     QSettings& settings;
     QNetworkAccessManager& network;
 
     QTcpServer authServer;
 
+    Info info;
+
     std::shared_ptr<UIElementBridge> authStateInfo;
     std::shared_ptr<UIElementBridge> loginButton;
 
     Setting<QString> oauthToken;
     Setting<QString> channel;
+
+    QWebSocket socket;
+
+    QTimer timerReconnect;
+    QTimer heartbeatTimer;
+    QTimer heartbeatAcknowledgementTimer;
 };
