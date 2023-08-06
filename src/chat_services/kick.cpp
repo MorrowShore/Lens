3d065efe0c22161b3cda14ca33159d128abb2c00
Alexander <example@mail.com>
2023-08-06 04:49:51 +0300

    Kick: badge

--- src/chat_services/kick.cpp
+++ src/chat_services/kick.cpp
@@ -487,7 +487,7 @@ QString Kick::parseBadge(const QJsonObject &basgeJson, const QString &defaultVal
         {
             file = ":/resources/images/kick/badges/sub_gifter_blue.svg";
         }
-        else if (count == 28) // TODO: ...28...
+        else if (count >= 28 && count <= 43) // TODO: ...28-43...
         {
             file = ":/resources/images/kick/badges/sub_gifter_purple.svg";
         }
